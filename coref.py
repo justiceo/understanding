f840e0ff25c7b6fb2c99056679f1efbac8cd9ff0
Justice Ogbonna <ebesco@yahoo.com>
2020-05-16 19:24:33 -0700

    Add coref file

---
+++ coref.py
@@ -0,0 +1,9 @@
+# Co-reference resolution.
+# Input is a text and output is a co-reference resolved text.
+# See https://spacy.io/universe/project/neuralcoref
+# Also stanford coref
+
+def resolve(text):
+    return text
+
+print(resolve("hello world"))
