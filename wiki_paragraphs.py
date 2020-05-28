7f6161c0fb113b0f28f340879f667cfa7009793f
Justice Ogbonna <ebesco@yahoo.com>
2020-05-27 19:00:13 -0700

    Make it easy to call wiki paragraphs from command line

--- wiki_paragraphs.py
+++ wiki_paragraphs.py
@@ -1,9 +1,11 @@
 # Usage: python3 -i wiki_paragraphas.py
+# Usage: python3 wiki_paragraphs.py --url=https://en.wikipedia.org/wiki/Oxygen > data/oxygen.txt
 import os
 import json
 import re
 import shutil
 from shutil import which
+
 
 def is_requirements_ok():
     """Check whether curl and unfluffly is on PATH and marked as executable."""
@@ -17,11 +19,13 @@ def is_requirements_ok():
 
     return True
 
+
 def raw_text(url):
     if not is_requirements_ok():
         return None
     data = os.popen('curl -s "{url}" | unfluff'.format(url=url))
     return json.load(data)["text"]
+
 
 def paragraphs(url):
     # If error, return empty list.
@@ -34,4 +38,22 @@ def paragraphs(url):
         text = re.sub(r"\[\d+\]", "", text)
 
     # Split into paragraphs.
-    return  text.split("\n\n")
+    return text.split("\n\n")
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", required=True,
+                        help="The URL to wiki page.")
+    parser.add_argument("--output", required=False,
+                        help="File to write text output")
+
+    args = parser.parse_args()
+    ps = paragraphs(args.url)
+    out = "\n\n".join(ps)
+    if args.output is not None:
+        with open(args.output, "w") as outfile:
+            outfile.write(out)
+    else:
+        print(out)
