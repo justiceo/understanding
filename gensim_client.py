1e9bdd0fa6ff8147eb6e2b6477e5476520593d48
Justice Ogbonna <ebesco@yahoo.com>
2020-05-30 13:13:01 -0700

    Add gensim server and client

---
+++ gensim_client.py
@@ -0,0 +1,22 @@
+from multiprocessing.managers import BaseManager
+
+
+def get_queues():
+    BaseManager.register('terms_queue')
+    BaseManager.register('similarities_queue')
+    m = BaseManager(address=('localhost', 9100), authkey=b'random auth')
+    m.connect()
+
+    shared_terms_queue = m.terms_queue()
+    shared_similarities_queue = m.similarities_queue()
+    return (shared_terms_queue, shared_similarities_queue)
+
+
+def resolve(terms):
+    (shared_terms_queue, shared_similarities_queue) = get_queues()
+    shared_terms_queue.put(terms)
+    return shared_similarities_queue.get()
+
+
+if __name__ == "__main__":
+    print(resolve(["barack", "obama"]))
