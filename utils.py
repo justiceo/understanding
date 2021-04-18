565cdbb8bd65d0334aa07f8bcdbacb1493d8c79b
Justice Ogbonna <justiceo@users.noreply.github.com>
2021-04-17 20:47:23 -0700

    Cleanup unused files

--- utils.py
+++ utils.py
@@ -48,3 +48,8 @@ def fix_punctuation(text):
     text = dash_space.sub(r'\1', text)
     
     return text
+
+
+# Returns true if number is of the form \d*[st|nd|rd|th], crash or false otherwise.
+# See explanation at https://codegolf.stackexchange.com/a/162322
+isOrdinalNum = lambda v:'hsnrhhhhhh'[(v[-4:-3]!='1')*int(v[-3])]in v
