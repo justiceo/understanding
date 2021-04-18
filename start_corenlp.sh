ea9d9adb14ca17d22d44cbe7df1e869a9ead3c15
Justice Ogbonna <justiceo@users.noreply.github.com>
2021-04-17 20:49:18 -0700

    Move all .py files into src/ directory

--- start_corenlp.sh
+++ start_corenlp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ../stanford-corenlp-4.0.0/ && java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
+cd ./stanford-corenlp-4.2.0/ && java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
